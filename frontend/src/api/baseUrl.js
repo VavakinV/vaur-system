fe0b574ead8366a3b0410409fdd68dc4a81d72fe
creatior <vartem2020@mail.ru>
2026-04-29 22:17:26 +0300

    cors fix

--- frontend/src/api/baseUrl.js
+++ frontend/src/api/baseUrl.js
@@ -1,19 +1,9 @@
-const FALLBACK_API_URL = "http://127.0.0.1:8000"
-
-function normalizeBaseUrl(url) {
-    return url.replace(/\/+$/, "")
-}
-
 export function getApiBaseUrl() {
     const configuredUrl = process.env.VUE_APP_API_URL
 
     if (configuredUrl) {
-        return normalizeBaseUrl(configuredUrl)
+        return configuredUrl.replace(/\/+$/, "")
     }
 
-    if (typeof window !== "undefined" && window.location?.origin) {
-        return normalizeBaseUrl(window.location.origin)
-    }
-
-    return FALLBACK_API_URL
+    return ""
 }
