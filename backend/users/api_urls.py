8a3916de4e71c5de4716ee2bca8ae44ff03b1e69
creatior <vartem2020@mail.ru>
2026-05-04 19:57:56 +0300

    reorganized api structure

---
+++ backend/users/api_urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from users.views import GroupView, UserDetailView, UserView
+
+
+urlpatterns = [
+    path('groups/', GroupView.as_view(), name='groups'),
+    path('groups/<int:pk>/', GroupView.as_view(), name='group'),
+    path('users/<int:pk>/detail/', UserDetailView.as_view(), name='user_detail'),
+    path('users/<int:pk>/', UserView.as_view(), name='user'),
+]
