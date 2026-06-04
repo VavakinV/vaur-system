8d89a786e6cef4de0b12b4819d686f8f866c8a05
creatior <vartem2020@mail.ru>
2026-06-04 19:24:31 +0300

    added works/user/{id} endpoint

--- backend/works/urls.py
+++ backend/works/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from works.views import WorkDetailView, WorkDocumentView, WorkShortView
+from works.views import UserWorksView, WorkDetailView, WorkDocumentView, WorkShortView
 
 
 urlpatterns = [
+    path('user/<int:user_pk>/', UserWorksView.as_view(), name='user-works'),
     path('<int:pk>/', WorkShortView.as_view(), name='work-short'),
     path('<int:pk>/detail/', WorkDetailView.as_view(), name='work-detail'),
     path('<int:pk>/document/', WorkDocumentView.as_view(), name='work-document'),
